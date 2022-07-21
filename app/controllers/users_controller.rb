e7a0196f55f1e0899b6c57c5900e257d92e343d9
Rohit Kathrotiya <rohitkathrotiya63@gmail.com>
2022-07-21 18:07:56 +0200

    Add functionality in user module

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,26 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to users_path
   end
 
   def edit
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'urls/index'
-  get 'urls/new'
-  get 'urls/edit'
-  get 'urls/show'
-  get 'users/index'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/show'
+  resources :urls, only: [:index, :new, :create, :show, :edit]
+  resources :users, only: [:index, :new, :create, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
