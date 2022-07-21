2abc0abd1f3cdd6d33790826b518eb38c3f53aed
Rohit Kathrotiya <rohitkathrotiya63@gmail.com>
2022-07-21 19:03:39 +0200

    Create form, Index and perform function in URL

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,14 @@
 class Url < ApplicationRecord
   belongs_to :user
+
+  validates :long_url, presence: true, length: { minimum: 10}
+  before_create :generate_short_url, :sanitize
+  def generate_short_url
+    rand(36**8).to_s(36)
+  end
+  def sanitize
+    long_url.strip!
+    sanitize_url = self.long_url.downcase.gsub(/(https?:\/\/)|(www\.)/,"")
+    "http://#{sanitize_url}"
+  end
 end
